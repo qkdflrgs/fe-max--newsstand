4f3f1a0d392ca36144f4672c356d0ccb764cbda3
qkdflrgs <qkdflrgs@gmail.com>
2023-06-02 21:08:07 +0900

    feat: 그리드 뷰 컴포넌트 수정
    img 요소 생성 시 alt 추가

--- src/js/components/gridView.ts
+++ src/js/components/gridView.ts
@@ -21,8 +21,9 @@ export class GridView {
   createGridContent() {
     for (let i = 0; i < NUMBER_OF_GRID_CONTENTS; i++) {
       const imgSrc: string = this.props.pressList[i].src;
+      const imgAlt: string = this.props.pressList[i].alt;
       const gridContent = createElement('div', { class: 'grid-view__content' });
-      const contentImg = createElement('img', { src: `${imgSrc}` });
+      const contentImg = createElement('img', { src: `${imgSrc}`, alt: `${imgAlt}` });
 
       gridContent.append(contentImg);
       this.element.append(gridContent);
