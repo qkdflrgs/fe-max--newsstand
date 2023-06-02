66fb97669cd2fd8518ec2243030318c2dafa17a1
qkdflrgs <qkdflrgs@gmail.com>
2023-06-02 14:17:14 +0900

    feat: 그리드뷰 컴포넌트 추가

---
+++ src/js/components/gridView.ts
@@ -0,0 +1,31 @@
+import { createElement } from '../utils.ts';
+import { GripProps } from '../type.ts';
+import { NUMBER_OF_GRID_CONTENTS } from '../constant.ts';
+
+export class GridView {
+  private props: GripProps;
+  private element: HTMLElement;
+
+  constructor(props: GripProps) {
+    this.props = props;
+    this.element = createElement('div', { class: 'grid-view' });
+    this.render();
+  }
+
+  render() {
+    const newsMain = document.querySelector('.main-contents');
+    this.createGridContent();
+    newsMain?.append(this.element);
+  }
+
+  createGridContent() {
+    for (let i = 0; i < NUMBER_OF_GRID_CONTENTS; i++) {
+      const imgSrc: string = this.props.pressList[i].src;
+      const gridContent = createElement('div', { class: 'grid-view__content' });
+      const contentImg = createElement('img', { src: `${imgSrc}` });
+
+      gridContent.append(contentImg);
+      this.element.append(gridContent);
+    }
+  }
+}
