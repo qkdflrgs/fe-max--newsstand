7effb6c71bdd79fe0e57ca8ff33a3ed18268f825
qkdflrgs <qkdflrgs@gmail.com>
2023-06-02 21:08:38 +0900

    feat: 메인 업데이트

--- src/js/main.ts
+++ src/js/main.ts
@@ -1,12 +1,22 @@
+import fetchedData from '../../db.json';
+import { createStore } from './store.ts';
+import { GridView } from './components/gridView.ts';
 import { Header } from './components/header.ts';
 import { RollingNews } from './components/rollingNews.ts';
 import { Tapview } from './components/tapview.ts';
+import { MainContents } from './components/mainContents.ts';
+
+const store = createStore;
 
 const init = () => {
   const header = new Header(HeaderProps);
   const rollingNews = new RollingNews(RollingNewsProps);
   const tapview = new Tapview(TapviewProps);
+  const mainContents = new MainContents(MainContentsProps);
+  const gridView = new GridView(GripProps);
 };
+
+window.addEventListener('DOMContentLoaded', init);
 
 const systemDate: string = new Date().toLocaleDateString('ko-KR', {
   year: 'numeric',
@@ -32,4 +42,11 @@ const TapviewProps = {
   subscribeBtn: '내가 구독한 언론사',
 };
 
-init();
+const GripProps = {
+  pressList: [...fetchedData.pressList],
+};
+
+const MainContentsProps = {
+  prevBtnImg: './src/asset/icon/LeftButton.svg',
+  nextBtnImg: './src/asset/icon/RightButton.svg',
+};
