1319fa6c59225065b96bfd0d8263006ff85b0d1e
qkdflrgs <qkdflrgs@gmail.com>
2023-06-14 10:41:52 +0900

    feat: 액션 추가

---
+++ src/js/action.ts
@@ -0,0 +1,13 @@
+import { Action } from './type';
+
+export const setGridViewAction: Action = { type: 'SET_GRID_VIEW' };
+
+export const setListViewAction: Action = { type: 'SET_LIST_VIEW' };
+
+export const selectAllPressAction: Action = { type: 'SELECT_ALL_PRESS' };
+
+export const selectSubscribePressAction: Action = { type: 'SELECT_SUBSCRIBE_PRESS' };
+
+export const nextGridPageAction: Action = { type: 'NEXT_GRID_PAGE' };
+
+export const prevGridPageAction: Action = { type: 'PREV_GRID_PAGE' };
