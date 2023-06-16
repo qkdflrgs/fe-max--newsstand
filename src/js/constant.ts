1f1a8dab27b3e6aa6f07f3b5bc8c016ac28528b8
qkdflrgs <qkdflrgs@gmail.com>
2023-06-16 14:00:21 +0900

    feat: 카테고리 상수 추가

--- src/js/constant.ts
+++ src/js/constant.ts
@@ -1,3 +1,4 @@
 export const NUMBER_OF_GRID_CONTENTS: number = 24;
 export const FIRST_GRID_PAGE: number = 0;
 export const LAST_GRID_PAGE: number = 3;
+export const CATEGORY = ['종합/경제', '방송/통신', 'IT', '영자지', '스포츠/연예', '매거진/전문지', '지역'];
